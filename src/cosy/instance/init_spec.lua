66d8df012107c32f5cf5e3e484ddb140ef457249
Alban Linard <alban@linard.fr>
2016-11-18 10:31:39 +0100

    Fix test for instance, using dev server.

--- src/cosy/instance/init_spec.lua
+++ src/cosy/instance/init_spec.lua
@@ -12,9 +12,22 @@ local Instance = require "cosy.instance"
 
 describe ("instance", function ()
 
-  it ("should work", function ()
-    local instance = Instance.create ()
-    local server   = instance.server
+  it ("should work for dev branch", function ()
+    local instance = Instance.create {
+      branch      = "dev",
+      num_workers = 1,
+      auth0       = {
+        domain        = assert (os.getenv "AUTH0_DOMAIN"),
+        client_id     = assert (os.getenv "AUTH0_ID"    ),
+        client_secret = assert (os.getenv "AUTH0_SECRET"),
+        api_token     = assert (os.getenv "AUTH0_TOKEN" ),
+      },
+      docker      = {
+        username = assert (os.getenv "DOCKER_USER"  ),
+        api_key  = assert (os.getenv "DOCKER_SECRET"),
+      },
+    }
+    local server = instance.server
     instance:delete ()
     assert.is_truthy (server)
   end)
